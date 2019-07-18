d131e2cd22d398c7c8852242f196dc6b7e9a9d1e
Wes Jones <wesjones@Wess-MacBook-Pro.local>
2019-07-18 13:59:38 -0400

    added mongoDB Atlas for persisting data; added delete and show routes for todos

--- app.js
+++ app.js
@@ -1,32 +1,82 @@
 var bodyParser  = require("body-parser"),
     express     = require("express"),
-    app         = express()
-    // mongoose    = require("mongoose")
+    app         = express(),
+    mongoose    = require("mongoose"),
+    methodOverride  = require("method-override");
+
     
-// var todoSchema = new mongoose.Schema({
-//     entry: String,
-//     details: String,
-//     taskComplete: {type: Boolean, default: false}
-// });
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
-var todos = ["Walk Tucker", "Take Shower", "Drink Coffee"];
+// var todos = ["Walk Tucker", "Take Shower", "Drink Coffee"];
 
+// App Config
+mongoose.connect("mongodb+srv://Wes:WPWDnvlErs3Dujne@todo-list-nefpq.mongodb.net/test?retryWrites=true&w=majority");
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
 
-app.get("/", function(req, res) {
-    res.render("home", {todos: todos});
+// Defining Mongoose Schema
+var todoSchema = new mongoose.Schema({
+    entry: String,
+    priority: Number,
+    details: String,
+    taskComplete: {type: Boolean, default: false}
 });
 
+var Todo = mongoose.model("Todo", todoSchema);
+
+
+// Home route
+app.get("/", function(req, res) {
+    Todo.find({}, function(err, todos) {
+        if (err) {
+            console.log("LOADING ERROR: " + err);
+        } else {
+            res.render("home", {todos: todos});
+        }
+    });
+});
+
+
+// New route
 app.get("/new", function(req, res) {
     res.render("new");
 });
 
+
+// Create route
 app.post("/", function(req, res) {
-    var newTodo = req.body.entry;
-    todos.push(newTodo);
-    res.redirect("/");
+    Todo.create(req.body.todo, function(err, newTodo) {
+        if (err) {
+            console.log("CREATE ERROR: " + err);
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
+
+// Show route 
+// Currently shows plain json object 
+app.get("/:id", function(req, res) {
+    Todo.findById(req.params.id, function(err, foundTodo) {
+        res.render("show", {todos:foundTodo});
+    });
+});
+
+// Edit route in future
+// Update route in future
+
+
+
+// Delete route
+app.delete("/:id", function(req, res) {
+    Todo.findByIdAndRemove(req.params.id, function(err) {
+        res.redirect("/");
+    });
 });
 
 
